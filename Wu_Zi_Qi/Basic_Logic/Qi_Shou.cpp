633a6862c6cf41c59360092a2c077f1270dd002b
blank997 <117527101+blank997@users.noreply.github.com>
2023-10-20 18:11:46 +0800

    Delete Wu_Zi_Qi directory

--- Wu_Zi_Qi/Basic_Logic/Qi_Shou.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by lenovo on 2023/10/16.
-//
-
-#include "Qi_Shou.h"
--- Wu_Zi_Qi/Defination.h
+++
@@ -1,7 +0,0 @@
-//
-// Created by lenovo on 2023/10/16.
-//
-#ifndef WU_ZI_QI_DEFINATION_H
-#define WU_ZI_QI_DEFINATION_H
-
-#endif //WU_ZI_QI_DEFINATION_H
